aa19c210047bbc3c9182e18e81525fc77e2d952f
LeleMaria1 <leticiaonofre01@gmail.com>
2023-06-26 00:42:38 -0300

    Commit =)

---
+++ rodape.php
@@ -0,0 +1,6 @@
+</div>
+<script type="text/javascript" src="./resources/lib/jquery-3.1.1.js"></script>
+<script type="text/javascript" src="./resources/lib/bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+<script type="text/javascript" src="./resources/common/js/crud.js"></script>
+</body>
+</html>
